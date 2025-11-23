582a0d5920eae6b869d48c63f046ed41d2628813
Salim Caner Kitir <ckitir.dev@gmail.com>
2025-11-23 15:41:25 +0300

    İlk minimalapi projemiz oluşturuldu sqlserver ve git ile uyumlu hale getirildi ve get metodu eklendi

---
+++ Data/AddDbContext.cs
@@ -0,0 +1,18 @@
+﻿
+    using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
+    using MinimalAPI.Models;
+
+    namespace MinimalAPI.Data
+    {
+        public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions<AppDbContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<Coupon> Coupons => Set<Coupon>();
+        }
+    }
+
--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalAPI.Data;
+using MinimalAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -14,31 +20,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.MapGet("api/coupon", (AppDbContext db) => Results.Ok(db.Coupons.ToList()));
+app.MapGet("api/coupon/{id}", (int id, AppDbContext db) =>
+{
+    var coupon = db.Coupons.Find(id);
+    return coupon != null ? Results.Ok(coupon) : Results.NotFound();
+});
+
+
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
